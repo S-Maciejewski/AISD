8fe5080f4c922d402b00a63006c6f43bcdaa5bd5
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2017-05-08 17:19:39 +0200

    Bugfix

--- C++ Algorytmika.cpp
+++ C++ Algorytmika.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-int m = floor(nodes*(nodes - 1)*sat / 2) + 1, sptr = 0;
+int m = floor(nodes*(nodes - 1)*sat / 2), sptr = 0;
 int ** tab, * stack;	//stos dla funkcji znajdowania cyklu
 	
 
@@ -18,45 +18,11 @@ void findEuler(int v)
 	for (int i = 0; i < nodes; i++)
 		while (tab[v][i])
 		{
-			tab[v][i]--;                 
-			tab[i][v]--;
+			tab[v][i]--, tab[i][v]--;
 			findEuler(i);
 		}
 	stack[sptr++] = v;
 }
-
-//Dla multigrafu
-
-//void generate() {
-//	random_device random;
-//	mt19937 gen(random());
-//	uniform_int_distribution<> distribution(0, nodes - 1);
-//
-//	int arcCounter = 0;
-//	for (int i = 0; i < nodes - 1; i++)
-//	{
-//		tab[i][i + 1]++, tab[i + 1][i]++;
-//		arcCounter++;
-//	}
-//
-//	tab[0][nodes - 1]++, tab[nodes - 1][0]++;
-//	arcCounter++;
-//
-//	while (arcCounter < m - 3)
-//	{
-//		int a = 0, b = 0, c = 0;
-//		do
-//		{
-//			a = distribution(gen), b = distribution(gen), c = distribution(gen);
-//		} while (a == b || a == c || b == c);
-//		tab[a][b]++, tab[b][a]++;
-//		tab[b][c]++, tab[c][b]++;
-//		tab[c][a]++, tab[a][c]++;
-//
-//		arcCounter+=3;
-//	}
-//
-//}
 
 void generate() {
 	random_device random;
@@ -81,11 +47,15 @@ void generate() {
 			do
 			{
 				a = distribution(gen), b = distribution(gen);
+				cout << "a, b generated" << endl;
 			} while (a == b);
 		while (tab[a][b] == 1);
 		do
 			do
+			{
 				c = distribution(gen);
+				cout << "c generated" << endl;
+			}
 			while (c == a || c == b);
 		while (tab[b][c] == 1 || tab[c][a] == 1);
 
@@ -95,6 +65,8 @@ void generate() {
 
 		arcCounter += 3;
 	}
+	cout << endl << "Graph generated successfully" << endl;
+	cout << "Edge amound: " << arcCounter << endl;
 }
 
 void findHamilton(int v)
@@ -114,6 +86,9 @@ void findHamilton(int v)
 
 int main()
 {
+	time_t start;
+	double eulerTime = 0, hamiltonTime = 0;
+
 	tab = new int *[nodes];            
 	stack = new int[m];
 
@@ -136,13 +111,17 @@ int main()
 		cout << endl;
 	}
 
+	start = clock();
 	findEuler(0);
+	eulerTime = (clock() - start) / (double)CLOCKS_PER_SEC;
+
 
 	//wypisywanie cyklu
 	cout << "Cykl Eulera :" << endl;
 	for (int i = 0; i < sptr; i++)
 		cout << stack[i] << ", ";
-	cout << endl;
+
+	cout << endl << "Czas dzialania: " << eulerTime << endl;
 
 	delete[] stack;
 	stack = new int[nodes];
@@ -152,6 +131,7 @@ int main()
 
 	delete[] tab;
 	delete[] stack;
+
 
 	return 0;
 }
