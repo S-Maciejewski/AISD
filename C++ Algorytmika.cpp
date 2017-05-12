ed6fe98683f84998c38a9eacda213edec2cf2ac9
jantechner <jantechner@live.com>
2017-05-13 01:40:00 +0200

    W sumie wszystko działa
    Trzeba tylko dopracować generator danych w zależności jaki mamy pomysł na tworzenie wykresów.

--- C++ Algorytmika.cpp
+++ C++ Algorytmika.cpp
@@ -1,193 +1,142 @@
-﻿#include "stdafx.h"
 #include <iostream>
-#include <cmath>
 #include <ctime>
 #include <random>
 #include <stack>
-#include <fstream>
 
-#define nodes 50
-#define sat 0.3
-#define steps 15
-#define step 10
-#define repeats 5
+#define CAPACITY 1000
+#define ITEM_NUMBER 2000
 
 using namespace std;
-
-stack<int> usedNodes; 
-stack<int> resultEuler;
-stack<int> resultHamilton;
-int edges;
-int *visited;
-bool hamiltonFound = false;
-
-int ** eulerGraph;	
-int ** hamiltonGraph;
-
-void setGlobal(int nodeAmount)
-{
-	edges = int(floor(nodeAmount*(nodeAmount - 1)*sat / 2));
-	//visited = new int[nodeAmount]{ false };
-	visited = new int[nodeAmount];
-	for (int i = 0; i < nodeAmount; i++)
-		visited[i] = false;
+stack <int> greedy_results;
+stack <int> dynamic_results;
+struct item{
+    int value;
+    int weight;
+};
+void print_items(item ** tab, int item_number){
+    for(int i=0; i<item_number; i++)
+    {
+        cout<<"Item "<<i<<" : "<<tab[i]->value<<" "<<tab[i]->weight<<endl;
+    }
 }
 
-void generate() {
+void generate(item ** tab, int item_number, int capacity){
+    int weight_range = 20
+    /*(capacity/item_number)*3*/;
 
-	int arcCounter = 0;
-	for (int i = 0; i < nodes - 1; i++)
-	{
-		eulerGraph[i][i + 1]++, eulerGraph[i + 1][i]++;
-		arcCounter++;
-	}
+    int value_range = item_number;
+    int weight_sum = 0;
+    for(int i=0; i<item_number; i++)
+    {
+        tab[i]->value = (rand()%value_range) + 1;
+        tab[i]->weight = (rand()%weight_range) + 1;
+        weight_sum += tab[i]->weight;
+    }
 
-	eulerGraph[0][nodes - 1]++, eulerGraph[nodes - 1][0]++;
-	arcCounter++;
-
-	while (arcCounter <= edges - 3) {
-		int a = 0, b = 0, c = 0;
-		a = rand() % nodes, b = rand() % nodes;
-		while (eulerGraph[a][b] == 1 || a == b) {
-			a = rand() % nodes, b = rand() % nodes;
-		}
-
-		c = rand() % nodes;
-		while ((c == a || c == b) || (eulerGraph[b][c] == 1 || eulerGraph[c][a] == 1)) {
-			c = rand() % nodes;
-		}
-
-		eulerGraph[a][b]++, eulerGraph[b][a]++;
-		eulerGraph[b][c]++, eulerGraph[c][b]++;
-		eulerGraph[c][a]++, eulerGraph[a][c]++;
-
-		arcCounter += 3;
-	}
-
-
-	for (int i = 0; i < nodes * nodes; i++)
-	{
-		int randNode1 = rand() % nodes;
-		int randNode2 = rand() % nodes;
-		while (randNode1 == randNode2) {
-			randNode2 = rand() % nodes;
-		}
-		for (int k = 0; k<nodes; k++) swap(eulerGraph[randNode1][k], eulerGraph[randNode2][k]);
-		for (int j = 0; j<nodes; j++) swap(eulerGraph[j][randNode1], eulerGraph[j][randNode2]);
-	}
-
-	cout << endl << "Graph generated successfully" << endl;
-	cout << "Edge amount: " << arcCounter << " " << edges << endl << endl;
+    cout<<"Draw successfully ended"<<endl<<"Weight sum = "<<weight_sum<<endl;
+    if(weight_sum <= capacity) generate(tab, item_number, capacity);
 }
 
-int nextPath(int node) {
-	for (int i = 0; i < nodes; i++)
-	{
-		if (eulerGraph[node][i]) return i;
-	}
-	return -1;
+int greedy_algorithm(item ** tab, int item_number, int capacity){
+    int max_value = 0;
+    int weight_left = capacity;
+    for(int i=0; i<item_number; i++){
+        if(tab[i]->weight <= weight_left){
+            weight_left -= tab[i]->weight;
+            greedy_results.push(i);
+            max_value += tab[i]->value;
+        }
+        if(weight_left <= 0) return max_value;
+    }
+    return max_value;
 }
 
-void findEuler() {
-	int currentNode = 0;
-	int nextNode;
-	usedNodes.push(currentNode);
-	while (!usedNodes.empty())
-	{
-		if (nextPath(currentNode) != -1)
-		{
-			nextNode = nextPath(currentNode);
-			usedNodes.push(nextNode);
-			eulerGraph[currentNode][nextNode]--; eulerGraph[nextNode][currentNode]--;
-			currentNode = nextNode;
-		}
-		else
-		{
-			resultEuler.push(currentNode);
-			usedNodes.pop();
-			if (!usedNodes.empty()) currentNode = usedNodes.top();
-		}
-	}
+int dynamic_algorithm(item ** tab, int item_number, int capacity){
+    int ** matrix = new int *[item_number+1];
+    for(int i=0; i<=item_number; i++)
+    {
+        matrix[i] = new int[capacity+1];
+    }
+
+    //zerowanie zerowej kolumny i zerowego wiersza
+    for(int i=0; i<capacity+1; i++) matrix[0][i] = 0;
+    for(int i=0; i<item_number+1; i++) matrix[i][0] = 0;
+
+    //dynamiczny algorytm plecakowy
+    for(int i=1; i<=item_number; i++){
+        for(int j=1; j<=capacity; j++){
+            if(tab[i-1]->weight > j) matrix[i][j] = matrix[i-1][j];
+            else if(tab[i-1]->value + matrix[i-1][j-tab[i-1]->weight] > matrix[i-1][j]) matrix[i][j] = tab[i-1]->value + matrix[i-1][j-tab[i-1]->weight];
+            else matrix[i][j] = matrix[i-1][j];
+        }
+    }
+    //wyświetlanie macierzy dynamicznej
+//    cout<<endl<<"Dynamic matrix"<<endl;
+//    for(int i=0; i<item_number+1; i++){
+//        for(int j=0; j<capacity+1; j++){
+//            cout<<matrix[i][j]<<" ";
+//        }
+//        cout<<endl;
+//    }
+
+    //odczytywanie właściwych itemów
+    int i=item_number, j=capacity;
+    while(matrix[i][j]!=0)
+    {
+        while(matrix[i][j] == matrix[i-1][j]) i--;
+        while(matrix[i][j] == matrix[i][j-1]) j--;
+        dynamic_results.push(i-1);
+        j-= tab[i-1]->weight;
+        i--;
+    }
+
+    int max = matrix[item_number][capacity];
+    for(int i=item_number; i>0; i--)
+    {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+    return max;
 }
 
-void findHamilton(int v) {
-	visited[v] = true;
-	resultHamilton.push(v);
-	for (int i = 0; i < nodes; i++)
-		if (hamiltonGraph[v][i] && !visited[i]) {
-			hamiltonGraph[v][i]--; hamiltonGraph[i][v]--;
-			findHamilton(i);
-			if (hamiltonFound) return;
-			hamiltonGraph[v][i]++; hamiltonGraph[i][v]++;
-		}
+int main() {
+    srand(time(NULL));
+    int capacity = CAPACITY;
+    int item_number = ITEM_NUMBER;
+    int proba = 1000;
+    int max_value_greedy, max_value_dynamic;
+    struct item ** tab = new item* [item_number];
+    for(int i=0; i<item_number; i++)
+        tab[i] = new item;
+    double errors_sum = 0;
+    for(int i=0; i<proba; i++) {
+        generate(tab, item_number, capacity);
 
-	if (resultHamilton.size() == nodes && hamiltonGraph[v][0]) {
-		resultHamilton.push(0);
-		hamiltonFound = true;
-		return;
-	}
-	else {
-		resultHamilton.pop();
-		visited[v] = false;
-	}
-}
+//    print_items(tab, item_number);
 
-int main()
-{
-	time_t start;
-	srand(time(NULL));
-	double eulerTime = 0, hamiltonTime = 0;
-	short int eulerMultiplier = 10, hamiltonMultiplier = 4;
-	ofstream out ("wyniki.txt");
-	out << "nodes, eulerTime, hamiltonTime" << endl;
+        max_value_greedy = greedy_algorithm(tab, item_number, capacity);
+//    cout<<"Items by greedy algorithm"<<endl;
+//    while(!greedy_results.empty())
+//    {
+//        cout<<greedy_results.top()<<" ";
+//        greedy_results.pop();
+//    }
+//    cout<<endl;
+        //cout<<"Max value by greedy algorithm : "<<max_value_greedy<<endl;
 
-	for (int stepNumber = 0; stepNumber < steps; stepNumber++)
-	{
-		int nodesActually = nodes + stepNumber*step;
+        max_value_dynamic = dynamic_algorithm(tab, item_number, capacity);
+//    cout<<endl<<"Items by dynamic algorithm"<<endl;
+//    while(!dynamic_results.empty())
+//    {
+//        cout<<dynamic_results.top()<<" ";
+//        dynamic_results.pop();
+//    }
+//    cout<<endl;
+        //cout<<"Max value by dynamic algorithm : "<<max_value_dynamic<<endl;
 
-		setGlobal(nodesActually);
-		for (int repeatNumber = 0; repeatNumber < repeats; repeatNumber++)
-		{
-			eulerGraph = new int *[nodesActually];
-			for (int i = 0; i < nodesActually; i++)
-			{
-				eulerGraph[i] = new int[nodesActually];
-				for (int j = 0; j < nodesActually; j++)
-					eulerGraph[i][j] = 0;
-			}
-
-			generate();
-
-			hamiltonGraph = new int *[nodesActually];
-			for (int i = 0; i < nodesActually; i++)
-			{
-				hamiltonGraph[i] = new int[nodesActually];
-				for (int j = 0; j < nodesActually; j++)
-					hamiltonGraph[i][j] = eulerGraph[i][j];
-			}
-
-
-			start = clock();
-			for(short int i=0;i<eulerMultiplier;i++)
-				findEuler();
-			eulerTime = ((clock() - start) / (double)CLOCKS_PER_SEC) / (double)eulerMultiplier;
-
-			start = clock();
-			for (short int i = 0; i<hamiltonMultiplier; i++)
-				findHamilton(0);
-			hamiltonTime = (clock() - start) / (double)CLOCKS_PER_SEC / (double)hamiltonMultiplier;
-
-			delete[] eulerGraph;
-			delete[] hamiltonGraph;
-		}
-
-		eulerTime /= repeats;
-		hamiltonTime /= repeats;
-		cout << endl << "Euler time for " << nodesActually << " nodes : " << eulerTime << endl;
-		cout << endl << "Hamilton time for " << nodesActually << " nodes : " << hamiltonTime << endl;
-
-		out << nodesActually << ", " << eulerTime << ", " << hamiltonTime << endl;
-	}
-
-	return 0;
+        double relative_error = (max_value_dynamic - max_value_greedy) / double(max_value_dynamic);
+        errors_sum += relative_error;
+    }
+    cout<<endl<<"Relative error : "<<errors_sum / 1000<<endl;
+    return 0;
 }
